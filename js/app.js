ff4d42c7bc5feae1e81e0b59d9a4129acf1f3aab
salman602 <salmanrahman602@gmail.com>
2021-09-13 10:22:41 +0600

    Mandetory bugs fixed

--- js/app.js
+++ js/app.js
@@ -7,9 +7,12 @@ const loadProducts = () => {
 
 // show all product in UI 
 const showProducts = (products) => {
+  // console.log(products)
   const allProducts = products.map((pd) => pd);
+  // console.log(allProducts)
   for (const product of allProducts) {
-    const image = product.images;
+    console.log(product)
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -18,6 +21,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h5>${product.rating.rate} Out of 5</h5>
+      <h5>${product.rating.count} global ratings</h5>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -36,7 +41,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -45,7 +50,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -68,6 +73,7 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+  updateTotal();
 };
 
 //grandTotal update function
@@ -77,4 +83,5 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+// updateTotal();
 loadProducts();
